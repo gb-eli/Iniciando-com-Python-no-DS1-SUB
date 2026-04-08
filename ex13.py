26e0e07e6db75fc11921609297eb42898bd39dcb
Gabriel Elienai <162762339+gb-eli@users.noreply.github.com>
2026-04-08 09:03:40 -0300

    corrigindo lógica

--- ex13.py
+++ ex13.py
@@ -4,7 +4,7 @@ nota = float(input("Digite a nota: "))
 
 if nota >= 18:
     print("Aprovadoooo!")
-elif nota >= 8:
+elif nota >= 8 and nota < 17:
     print("Ficou na RCP")
 else:
     print("Reprovouuu")
